0e047895384ef8bdd11a1f6669ff64b067e9e817
Dcarrasc <David.carrascoo@usm.cl>
2025-10-10 20:32:19 -0300

    Update Jugador.java
    Reestructuración de la clase para que se entienda mejor. Se añaden las implementaciones extra para gestionar el inventario.

--- player/Jugador.java
+++ player/Jugador.java
@@ -21,47 +21,40 @@ public class Jugador implements AccesoProfundidad {
     private NaveExploradora nave;
     private boolean trajeTermico;
     private boolean mejoraTanque;
+    private boolean moduloInstalado;
     private boolean juegoCompletado = false;
 
+    /**
+     * Constructor de la Clase
+     * @param oxigeno tipo: oxígeno; Descripción: Capacidad del personaje para respirar bajo el agua
+     */
     public Jugador(Oxigeno oxigeno){
         this.tanqueOxigeno = oxigeno;
         this.profundidadActual = 0;
         this.tienePlanos = false;
         this.mejoraTanque = false;
         this.trajeTermico = false;
+        this.moduloInstalado = false;
         this.inventario = new ArrayList<>();
     }
 
+    //*******************************************
+    //*     Estado y propiedades del jugador    *
+    //*******************************************
+
     /**
-     * Ver el estado del Jugador
+     * Imprime por pantalla el estado del jugador
      */
     public void verEstadoJugador(){
+        System.out.println("\n===   Estado Jugador  ===");
         System.out.println("Profundidad: " + profundidadActual + " m");
-        System.out.println("Zona: " + zonaActual);
         System.out.println("Oxígeno: " + tanqueOxigeno.getOxigenoRestante() + "/" + tanqueOxigeno.getCapacidadMaxima());
-        System.out.println("Mejora tanque: " + mejoraTanque);
-        System.out.println("TrajeTermico: " + trajeTermico);
-        if (inventario.isEmpty()) {
-            System.out.println("Inventario: (vacío)");
-        }else {
-            System.out.println("Inventario:");
-            for (Item it : inventario) {
-                System.out.println(" - " + it);
-            }
-        }
-    }
-
-
-    public void profundidadActualizar(int profundidad_nueva, Zona zona){
-        int delta = Math.abs(profundidad_nueva - this.profundidadActual);
-        double d = zona.normalizarProfundidad(profundidad_nueva);
-        int costo = FormulaO2.cMover(d, delta);
-
-
-        tanqueOxigeno.consumirO2(costo);
-        this.profundidadActual = profundidad_nueva;
-
-        System.out.println("Movimiento a Profundidad: " + profundidad_nueva + " m, costo de O2: " + costo);
+        System.out.println("Mejora Tanque: " + (mejoraTanque ? "Sí" : "No"));
+        System.out.println("Traje Térmico: " + (trajeTermico ? "Sí" : "No"));
+        System.out.println("Modulo Instalado: " + (moduloInstalado ? "Sí" : "No"));
+        System.out.println("Planos: " + (tienePlanos ? "Sí" : "No"));
+        System.out.println("Zona: " + zonaActual);
+        verInventario();
     }
 
     @Override
@@ -73,49 +66,8 @@ public class Jugador implements AccesoProfundidad {
             // Zona Profunda
             return this.mejoraTanque;
         } else {
-            //Arrecife
+            //Arrecife y Nave estrellada
             return true;
-        }
-    }
-
-    public Oxigeno getTanqueOxigeno() {
-        return tanqueOxigeno;
-    }
-
-    public boolean isMejoraTanque() {
-        return mejoraTanque;
-    }
-    public void setMejoraTanque(boolean mejoraTanque) {
-        this.mejoraTanque = mejoraTanque;
-    }
-
-    public void setTienePlanos(){
-        this.tienePlanos = true;
-    }
-    public boolean isTienePlanos() {
-        return tienePlanos;
-    }
-
-    public int getProfundidadActual() {
-        return profundidadActual;
-    }
-    public void setTrajeTermico (){
-        this.trajeTermico = true;
-    }
-    public boolean isTrajeTermico() {
-        return trajeTermico;
-    }
-    public boolean tieneModuloProfundidad(){
-        return nave.getModuloProfundidad().isActivo();
-    }
-
-    public void agregarItem(ItemTipo tipo, int cantidad){
-        for (Item i : inventario) {
-            if (i.getTipo() == tipo){
-                i.setCantidad(i.getCantidad()+cantidad);
-                return;
-            }
-            inventario.add(new Item(tipo,cantidad));
         }
     }
 
@@ -133,32 +85,147 @@ public class Jugador implements AccesoProfundidad {
         }
     }
 
-    public int contarItem(ItemTipo tipo) {
-        int total = 0;
-        for (Item item : inventario) {
-            if (item.getTipo() == tipo) {
-                total += item.getCantidad();
-            }
-        }
-        return total;
-    }
+    //***********************
+    //*     Inventario      *
+    //***********************
 
-    private void consumirItem(ItemTipo tipo,int cantidad){
+    /**
+     * Agrega un item al inventario.
+     * @param tipo tipo: ItemTipo; descripción: Tipo del item que se agregará al inventario
+     * @param cantidad tipo: int; descripción: Cantidad del item que se agregará al inventario
+     */
+    public void agregarItem(ItemTipo tipo, int cantidad){
+        if (cantidad <= 0) return;
+
         for (Item i : inventario) {
-            if (i.getTipo() == tipo) {
-                i.setCantidad(i.getCantidad()-cantidad);
-                if (i.getCantidad() <= 0){
-                    inventario.remove(i);
-                }
+            if (i.getTipo() == tipo){
+                i.setCantidad(i.getCantidad()+cantidad);
                 return;
             }
+            inventario.add(new Item(tipo,cantidad));
         }
     }
 
+    /**
+     * Función para consumir objetos del inventario.
+     * @param tipo tipo: ItemTipo; descripción: Tipo del item que se quiere contar.
+     * @return tipo: boolean; descripción: Retorna verdadero consumio todos los items y falso si aún le queda en el inventario
+     */
+    public boolean consumirItem(ItemTipo tipo,int cantidad){
+        for (Item i : inventario) {
+            if (i.getTipo() == tipo) {
+                if (i.getCantidad() >= cantidad) {
+                    i.setCantidad(i.getCantidad() - cantidad);
+                    if (i.getCantidad() == 0) inventario.remove(i);
+                    return true;
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Cuenta cuántas unidades de un tipo tiene el jugador
+     * @param tipo tipo: ItemTipo; descripción: Tipo del item que se quiere contar.
+     * @return tipo: int; descripción: cantidad entera del tipo de item que tiene el jugador
+     */
+    public int contarItem(ItemTipo tipo) {
+        for (Item i : inventario) {
+            if (i.getTipo() == tipo) {
+                return i.getCantidad();
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Muestra el inventario que posee el jugador por pantalla.
+     */
+    public void verInventario(){
+        System.out.println("\n==== Inventario del Jugador ===");
+        if (inventario.isEmpty()){
+            System.out.println("Inventario vacio");
+        }
+        for (Item i : inventario) {
+            System.out.printf("- %s: %d%n", i.getTipo(), i.getCantidad());
+        }
+    }
+
+    /**
+     * Cuando el jugador muere en una expedición se pierde el inventario
+     */
+    public void vaciarInventario(){
+        inventario.clear();
+    }
+
+    //***********************************
+    //* Getter y Setters del jugador    *
+    //***********************************
+    public Oxigeno getTanqueOxigeno() {
+        return tanqueOxigeno;
+    }
+
+    public int getProfundidadActual() {
+        return profundidadActual;
+    }
+
+    public boolean tieneModuloProfundidad(){
+        if (nave.getModuloProfundidad().isActivo()){
+            setModuloInstalado(true);
+        }
+        return moduloInstalado;
+    }
+    public void setModuloInstalado(boolean valor) {
+        this.moduloInstalado = valor;
+    }
+    public boolean isMejoraTanque() {
+        return mejoraTanque;
+    }
+    public void setMejoraTanque(boolean mejoraTanque) {
+        this.mejoraTanque = mejoraTanque;
+    }
+    public boolean isTrajeTermico() {
+        return trajeTermico;
+    }
+    public void setTrajeTermico (){
+        this.trajeTermico = true;
+    }
+    public boolean isTienePlanos() {
+        return tienePlanos;
+    }
+    public void setTienePlanos(){
+        this.tienePlanos = true;
+    }
+    public List<Item> getInventario() {
+        return inventario;
+    }
     public boolean isJuegoCompletado() {
         return juegoCompletado;
     }
     public void setJuegoCompletado(boolean juegoCompletado) {
         this.juegoCompletado = juegoCompletado;
     }
+     // ********************
+     // * Otros métodos    *
+     // ********************
+
+    /**
+     * Función para que el jugador pueda moverse a nado dentro de la zona, no se permite cambiar de zona a nado.
+     * @param profundidad_nueva tipo: int; descripción: Cantidad en metros que se está moviendo el personaje
+     * @param zona tipo: Zona; descripción: Zona actual del personaje.
+     */
+     public void profundidadActualizar(int profundidad_nueva, Zona zona){
+        if (profundidad_nueva < zona.getProfundidadMin() || profundidad_nueva > zona.getProfundidadMax()){
+            System.out.println("Esa profundidad está fuera del rango permitido de la zona.");
+        }
+         int delta = Math.abs(profundidad_nueva - this.profundidadActual);
+         double d = zona.normalizarProfundidad(profundidad_nueva);
+         int costo = FormulaO2.cMover(d, delta);
+
+         tanqueOxigeno.consumirO2(costo);
+         this.profundidadActual = profundidad_nueva;
+
+         System.out.println("Movimiento a Profundidad: " + profundidad_nueva + " m, costo de O2: " + costo);
+     }
 }
